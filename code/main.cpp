05f804db5b26ed538ff974aea841c8d17874187a
Christian Ledgard <39962750+christianledgard@users.noreply.github.com>
2020-06-26 19:07:05 -0500

    Update main.cpp

--- code/main.cpp
+++ code/main.cpp
@@ -4,14 +4,6 @@
 #include "Earley.cpp"
 
 int main() {
-//	gramatica suma;
-//	suma.crearRegla("S","N+S|N*S|(S)|N");
-//	suma.crearRegla("N","1|2|3|4|5|6|7|8|9");
-//
-//	Earley sumas(&suma,"1+2+(3*4)");
-//	cout<<sumas.reconocer()<<endl<<endl;
-//    cout <<"|-----------------------------------------------------------------------|\n";
-
     gramatica g;
     g.crearRegla("S","AB");
     g.crearRegla("A","CD|CF");
@@ -31,37 +23,6 @@ int main() {
 
 	Earley gram1(&g,"aaabbbcc");
 	cout<<gram1.reconocer()<<endl<<endl;
-
-
-    gramatica a;
-    a.crearRegla("S","RT");
-    a.crearRegla("R","TR|a");
-    a.crearRegla("T","TR|b");
-    a.print();
-
-    CYK cyk2(&a,'S',"baba");
-
-
-
-    cout <<"|-----------------------------------------------------------------------|\n";
-    gramatica clase;
-    clase.crearRegla("S","AB|BC");
-    clase.crearRegla("A","BA|a");
-    clase.crearRegla("B","CC|b");
-    clase.crearRegla("C","AB|a");
-    clase.print();
-
-    CYK cyk3(&clase,'S',"baaba");
-    cyk3.solve();
-    cout << cyk3.cadenaAceptada()<< endl;
-    cout << cyk3;
-
-    cout <<"|-----------------------------------------------------------------------|\n";
 	
-	Earley gram2(&clase,"baaba");
-	cout<<gram2.reconocer()<<endl<<endl;
-    Earley e(&clase,"baaba");
-    cout << e.reconocer() << endl;
-
     return 0;
 }
